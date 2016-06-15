3dfafe5fd562f0041961d1c12668198262dfea18
Jak71004 <Jonathan@CandiedYamPublishing.com>
2016-06-15 08:38:18 -0400

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,2 @@
+// public/js/app.js
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'AdminCtrl', 'AdminService', 'SignUpService']);
---
+++ public/js/appRoutes.js
@@ -0,0 +1,21 @@
+// public/js/appRoutes.js
+
+angular.module('appRoutes',[]).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider
+    //home page
+    .when('/', {
+        templateUrl: './../views/home.html',
+        controller: 'MainController'
+    })
+    .when('/AdminHome', {
+        templateUrl: './../views/AdminHome.html',
+        controller: 'AdminController'
+    })
+    .when('/SignUp', {
+        templateUrl: './../views/signUp.html',
+        //controller: 'SignUpController'
+    });
+    
+    $locationProvider.html5Mode(true);
+    
+}]);
